dd8f13ce24a3c8c11003cda5d72b26095e03263c
Richard Ascanio C <richardascanio@correo.unimet.edu.ve>
2021-03-25 12:43:12 -0400

    styling bottom nav view

--- app/src/main/java/com/richard/cinemapp/utils/Constants.kt
+++ app/src/main/java/com/richard/cinemapp/utils/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
 
     // The MovieDB API
     const val BASE_URL = "https://api.themoviedb.org/3"
-    const val BASE_IMAGE_URL = ""
+    const val BASE_IMAGE_URL = "https://image.tmdb.org/t/p/regular"
     const val REQUEST_TIMEOUT = 15L
 
 }
